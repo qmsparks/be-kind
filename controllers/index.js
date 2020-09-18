aeafaf31a43388625c5335efa2cb7881bc308119
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-18 13:12:48 -0700

    Adds base for and imports controllers into server.js

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    auth: require('./auth'),
+    message: require('./message'),
+    nudge: require('./nudge')
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
 
+const controllers = require('./controllers');
 const app = express();
 
 const PORT = 3000;
