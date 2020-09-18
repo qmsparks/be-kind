ca538df452f31b129d39194ed8574bc0e2198482
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-18 12:56:38 -0700

    Creates models and controllers files for Users, nudges and messages

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Now listening for requests on port ${PORT}`);
