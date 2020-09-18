e51c282e5df8c3ee7ad01476bc5327e2c88ae588
qsparks <q.m.sparks@gmail.com>
2020-09-18 15:26:33 -0500

    builds initial schema for user, message, and nudge models

--- models/Nudge.js
+++ models/Nudge.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const nudgeSchema = new Schema({
-
-});
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  taskName: {type: String, required: true},
+  taskDescription: String,
+  scheduledFor: {type: Date, required: true}
+}, {timestamps: true});
 
 const Nudge = mongoose.model('Nudge', nudgeSchema);
 
--- models/User.js
+++ models/User.js
@@ -2,8 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
-})
+  name: {type: String, required: true},
+  email: {type: String, unique: true},
+  phone: {type: String, unique: true},
+  password: {type: String, required: true},
+  sendOnce: Boolean,
+  messages: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message'
+  }],
+  nudges: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Nudge'
+  }]
+  }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
 
