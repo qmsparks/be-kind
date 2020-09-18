f3f0ae09a73b14ea923aafbcfcf52ed1eff58d8a
qsparks <q.m.sparks@gmail.com>
2020-09-18 16:14:25 -0500

    initializes routers in server and hits route to profile page

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,4 +3,11 @@ const router = express.Router();
 
 const db = require('../models');
 
+
+
+// show profile route
+router.get('/profile', (req, res) => {
+  res.send('See profile');
+})
+
 module.exports = router;
