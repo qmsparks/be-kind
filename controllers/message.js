4ca8ff9ea47bd536ff5523249bd0e6b9d5a558ed
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-19 09:34:05 -0700

    Moved /message route to message controller from auth controller

--- controllers/message.js
+++ controllers/message.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const db = require('../models');
 
+router.post('/message', (req, res) => {
+    var message = req.body.message;
+    db.Message.create
+    res.render('sign-up', {
+        myMessage: message
+    });
+});
+
 module.exports = router;
--- models/Message.js
+++ models/Message.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  content: {type: String, required: true},
-  receivedDaily: Boolean,
-  sent: {type: Boolean, default: false},
-  scheduledFor: Date
-}, {timestamps: true});
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  content: { type: String, required: true },
+  // receivedDaily: Boolean,
+  // sent: { type: Boolean, default: false },
+  // scheduledFor: Date
+}, { timestamps: true });
 
 const Message = mongoose.model('Message', messageSchema);
 
