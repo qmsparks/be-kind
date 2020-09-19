fa7cde9df82bc215859a5908140e0f7ae9fc39d8
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-19 09:39:56 -0700

    Deletes console.logs from index.ejs

--- controllers/message.js
+++ controllers/message.js
@@ -4,11 +4,21 @@ const router = express.Router();
 const db = require('../models');
 
 router.post('/message', (req, res) => {
+    const user = req.session;
+    console.log(user);
     var message = req.body.message;
-    db.Message.create
-    res.render('sign-up', {
-        myMessage: message
-    });
+
+    if (!user) {
+        console.log('No User');
+        return;
+    } else if (user) {
+        console.log('User exists');
+        return;
+    }
+    // db.Message.create
+    // res.render('sign-up', {
+    //     myMessage: message
+    // });
 });
 
 module.exports = router;
