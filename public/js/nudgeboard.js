b76f1aba82b61cb799c0dd089b8fad537cb55502
qsparks <q.m.sparks@gmail.com>
2020-09-19 15:30:31 -0500

    continuing to wrestle with calendar display

--- public/js/nudgeboard.js
+++ public/js/nudgeboard.js
@@ -1,22 +1,24 @@
-  const calendarEl = document.getElementById('calendar');
-  const calendar = new FullCalendar.Calendar(calendarEl, {
-    headerToolbar: false,
-    initialView: 'timeGridOneDay',
-    views: {
-      timeGridOneDay: {
-        type: 'timeGrid',
-        dayCount: 1
-      }
-    },
-    aspectRatio: .5,
-    allDaySlot: false,
-    dayHeaders: false,
-    dateClick: function(info) {
-      date = new Date(info.dateStr);
-      // const hour = thisDate.getHours();
-      // const minutes = thisDate.getMinutes();
-      $('input[name="scheduledFor"]').val(info.dateStr);
-      $('#nudge-modal').css('display', 'block');
+const calendarEl = document.getElementById('calendar');
+const calendar = new FullCalendar.Calendar(calendarEl, {
+  headerToolbar: false,
+  initialView: 'timeGridOneDay',
+  views: {
+    timeGridOneDay: {
+      type: 'timeGrid',
+      dayCount: 1
     }
-  });
+  },
+  aspectRatio: .5,
+  allDaySlot: false,
+  dayHeaders: false,
+  dateClick: function(info) {
+    date = new Date(info.dateStr);
+    $('input[name="scheduledFor"]').val(info.dateStr);
+    $('#nudge-modal').css('display', 'block');
+
+  }, 
+});
+
+$('#show-nudges').on('click', () => {
   calendar.render();
+})
