11175c02767772475e9d39be44cec9db7acbdc8e
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-18 19:11:08 -0700

    adds session and connect-mongo packages for authorization and authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
+const MongoStore = require('connect-mongo')(session);
 
 const controllers = require('./controllers');
 const app = express();
@@ -12,6 +13,20 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "hawksblood",
+    store: new MongoStore({
+        url: "mongodb://localhost:27017/be-kind-sessions",
+    }),
+    cookie: {
+        // milliseconds
+        // 1000 (one second) * 60 (one minute) * 60 (one hour) * 24 (one day) * 7 (one week) * 2
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2
+    }
+}));
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
     next();
