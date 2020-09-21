446aa57a03ed942f20d85f9ee7fa464e29882ac7
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-21 09:51:02 -0700

    Creates getRandomTimeOfWeek() function

--- cronSandbox.js
+++ cronSandbox.js
@@ -17,7 +17,8 @@ setTimeout(() => {
 // NOTE cron string = second*, minute, hour, day of month, month, day of week 
 // NOTE date string = year, monthIndex, day, hour, minute
 
-const playDate = new Date();
+const playDate = new Date('September 30, 2020, 3:24:00');
+
 
 const getCronValues = (createdAt) => {
     const minute = createdAt.getMinutes();
@@ -40,24 +41,39 @@ const sendNudge = (nudge) => {
 const getRandomTimeOfDay = () => {
     const minute = randomNumInRange(0, 59);
     const hour = randomNumInRange(9, 18);
-
     return `${minute} ${hour} * * *`;
 }
 
 
 
 const getRandomTimeOfWeek = (cronValues) => {
-    const vals = cronValues.split(' ');
-    const valNums = vals.map(val => {
-        return parseInt(val);
-    })
-    const date = valNums[2];
-
-
-
     const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    const minute = randomNumInRange(0 - 59);
-    const hour = randomNumInRange(9 - 17);
+    const valNums = cronValues.split(' ').map(val => parseInt(val));
+    const createDate = valNums[2];
+    const createDay = valNums[4];
+    const daysInMonth = days[valNums[3] - 1];
+
+    // if the user has receiveDailyMessages set to true, daysFromSend = 1 else :
+    const daysFromSend = Math.floor(Math.random() * 7);
+
+
+    if ((createDate + daysFromSend) > daysInMonth) {
+        valNums[2] = (createDate + daysFromSend) - daysInMonth;
+        valNums[3] += 1;
+    } else {
+        valNums[2] = createDate + daysFromSend;
+    }
+
+    if ((createDay + daysFromSend) > 6) {
+        valNums[4] = (createDay + daysFromSend) - 6;
+    } else {
+        valNums[4] = createDay + daysFromSend;
+    }
+
+    valNums[0] = randomNumInRange(0, 59);
+    valNums[1] = randomNumInRange(9, 17);
+
+    return valNums.join(' ');
 }
 
 
@@ -78,4 +94,5 @@ const schedule = (cronValues, transmission, daily = false) => {
 
 
 console.log(getRandomTimeOfDay());
+console.log(getCronValues(playDate));
 getRandomTimeOfWeek(getCronValues(playDate));
