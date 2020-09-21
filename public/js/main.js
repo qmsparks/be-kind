b553fdc08e742197c3afc38fbd9f659717d44cbb
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-21 14:29:49 -0700

    Adds typewriter effect to homepage

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
+const app = document.getElementById('question');
+const answer = $('#answer');
+const btn = $('.btn');
+
+var typewriter = new Typewriter(app, {
+    loop: false,
+    delay: 75,
+    cursor: ''
+});
+
+typewriter
+    .pauseFor(2500)
+    .typeString('Write something kind to yourself')
+    .start();
+
+const checkVal = () => {
+    setInterval(function () {
+        if (answer.val() !== '') {
+            btn.css('visibility', 'visible');
+            clearInterval();
+        }
+    }, 300);
+}
+
+checkVal();
+
 
