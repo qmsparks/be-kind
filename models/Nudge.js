5021f7f0af5d373be127fe0197a18fbb9d0ccb80
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-23 18:11:12 -0700

    Get's rid of superfluous comments and unused functions inside of Nudge schema.

--- models/Nudge.js
+++ models/Nudge.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
 const nudgeSchema = new Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -24,32 +26,12 @@ const nudgeSchema = new Schema({
     default: false
   }
 },
-{
-  timestamps: true
-});
-
-
-// nudgeSchema.methods.getCronString = function() {
-//   this.cronString = getCronValues(this.scheduledFor);
-//   this.save();
-// }
+  {
+    timestamps: true
+  });
 
 
 
-
-/**
- * @function getCronValues()
- * @description returns a cron string from date passed in
- * @param {Date object} date any Date object. 
- */
-const getCronValues = (date) => {
-  const minute = date.getMinutes();
-  const hour = date.getHours();
-  const dayOfMonth = date.getDate();
-  const month = date.getMonth() + 1;
-  const dayOfWeek = date.getDay();
-  return `${minute} ${hour} * * *`
-}
 
 const Nudge = mongoose.model('Nudge', nudgeSchema);
 
