73a82a9c2ca7281c6cc358871ea0d86758bc3f66
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-23 18:10:05 -0700

    Formats Message schema

--- models/Message.js
+++ models/Message.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
 const messageSchema = new Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -21,6 +23,8 @@ const messageSchema = new Schema({
 }, {
   timestamps: true
 });
+
+
 
 const Message = mongoose.model('Message', messageSchema);
 
