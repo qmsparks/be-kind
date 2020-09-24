b042c9c849126902973b019ed604d5808f889b50
jasonhawkharris <jasonhawkharris@gmail.com>
2020-09-23 18:09:24 -0700

    Formats index.js in models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,14 @@
-// modules
 const mongoose = require('mongoose');
 const connectionString = 'mongodb://localhost:27017/be-kind';
 
-// constants
 const DB_CONNECTED_MSG = `Connected to the Be-Kind database at ${connectionString}`;
 const DB_DISCONNECTED_MSG = `Disconnected from the Be-Kind database at ${connectionString}`;
 const DB_ERROR_MSG = `Error connectiong to the Be-Kind database at ${connectionString}`;
 
-// connections and feedback
+
+
+
+
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -16,14 +17,17 @@ mongoose.connect(connectionString, {
 }).then(() => {
     console.log(DB_CONNECTED_MSG);
 }).catch((err) => {
-    console.log(DB_ERROR_MSG);
+    console.log(DB_ERROR_MSG + ': ' + err);
 });
 
 mongoose.connection.on('disconnected', () => {
     console.log(DB_DISCONNECTED_MSG);
 });
 
-// exports
+
+
+
+
 module.exports = {
     User: require('./User'),
     Message: require('./Message'),
